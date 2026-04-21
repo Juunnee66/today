da19c15e45ecec98bfb60ba1fdf60dc9f95f142f
June <juunnee66@gamil.com>
2026-04-21 15:03:01 +0900

    20260421

---
+++ RISK_PREDICT/user/request.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+# 회원가입 요청에 필요한 데이터 형식
+class SignUpRequest(BaseModel):
+    email: str
+    password: str
+
+# 로그인에 필요한 데이터 형식
+class LogInRequest(BaseModel):
+    email: str
+    password: str
+
+
+class AuthRequest(BaseModel):
+    email: str
+    password: str
